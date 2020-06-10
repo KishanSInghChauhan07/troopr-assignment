f4602c0f875f027e699eaffa7edc4d900efe0d06
kishansinghchauhan <kishancoer7071@gmail.com>
2020-06-11 02:34:16 +0530

    add phone no

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Route,BrowserRouter,Switch } from 'react-router-dom';
+import InfoPage from './Components/InfoPage/InfoPage.Component';
+import HomePage from './Components/HomePage/HomePage.Component';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/Home' component= { HomePage } />
+        <Route exact path='/' component= { InfoPage } />
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
