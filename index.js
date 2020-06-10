316e07f023d60836dbb0f1a8440c5bad56599dd6
kishansinghchauhan <kishancoer7071@gmail.com>
2020-06-10 23:49:59 +0530

    add front end

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const Port = 7000;
+
+const {mongoUrl} = require('./keys')
+
+mongoose.connect(mongoUrl,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+mongoose.connection.on('connected', () =>{
+    console.log("connected to the mongodb");
+})
+mongoose.connection.on('error', (error) =>{
+    console.log("error connecting to the mongodb",err);
+})
+
+require('./models/data');
+app.use(express.json())
+app.use(require('./routes/data'));
+
+app.listen(Port,() => {
+    console.log("server is running on :", Port);
+    
+})
+
+
---
+++ routes/data.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router()
+const mongoose = require('mongoose');
+const Data = mongoose.model("Data")
+const bcrypt = require('bcryptjs');
+
+
+router.post('/',(req,res) => {
+    const {name,email,password,phone} = req.body
+    if(!email || !password || !name || !phone){
+        return res.status(422).json({error:"Please add all the fields"})
+    }
+    Data.findOne({email:email})
+    .then((savedUser) => {
+        if(savedUser){
+            return res.status(422).json({error:"user already exists"})
+        }
+        bcrypt.hash(password,12)
+        .then( hashedPassword => {
+            const data = new Data({
+                email,
+                name,
+                phone,
+                password:hashedPassword
+            })
+            data.save()
+            .then( user =>{
+                res.json({message:"saved succesfully"})
+            })
+        .   catch( err => {
+                console.log(err);
+
+            })
+        })
+        
+    })
+    .catch(err => {
+        console.log(err);
+        
+    })
+})
+
+
+module.exports = router
